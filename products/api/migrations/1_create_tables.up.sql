137d634387ac44059d0e2fd8268211e5894a5bf7
pranav-kural <pranavkural042@gmail.com>
2024-08-27 01:06:51 -0400

    Modified database tables - added categories + hero_products tables

--- products/api/migrations/1_create_tables.up.sql
+++ products/api/migrations/1_create_tables.up.sql
@@ -1,8 +1,25 @@
+CREATE TABLE categories (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    description TEXT,
+    image_url TEXT NOT NULL,
+    offered BOOLEAN NOT NULL
+);
+
 CREATE TABLE products (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL,
     description TEXT,
-    category TEXT NOT NULL,
-    imageUrl TEXT NOT NULL,
-    price INT NOT NULL
-  );
+    category_id BIGINT NOT NULL,
+    image_url TEXT NOT NULL,
+    price INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE hero_products (
+    category_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (category_id, product_id),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
